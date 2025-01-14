6850204bbb87dcf15fe9a1b5175ef9128262cfa2
Rusheel Koushik Gollakota <rusheel@Rusheels-MacBook-Pro.local>
2025-01-14 22:13:09 +0530

    implemented backend for middleware and login

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const User = require('../../models/User');
 
@@ -49,7 +51,17 @@ router.post('/',
         user.password = await bcrypt.hash(password, salt);
 
         await user.save();
-        res.send('User registered');
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+        jwt.sign(payload, config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err,token)=>{
+            if(err) throw err;
+            res.json({ token });
+        });
     }
     catch (err) {
         console.error(err.message);
